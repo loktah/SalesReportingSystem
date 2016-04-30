d480966ab5d85bd1117e9e108431715f060c424f
lowji <lowji107@gmail.com>
2016-04-30 14:46:38 +1000

    first submit

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<HTML>
+<head>
+<title>Sales Reporting System</title>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+<h1>Sales Reporting System</h1>
+<br/>
+<br/>
+<h3>Customer Management</h3>
+<p><a href="customer.php">Add New Customer</a></p>
+<br/>
+<h3>Product Management</h3>
+<p><a href="product.php">Add New Product</a></p>
+<br/>
+<h3>Sale Management</h3>
+<p><a href="sale.php">Add New Sale</a></p>
+</body>
+</HTML>
